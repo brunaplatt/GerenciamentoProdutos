4474a91ed98ce68ecac3f603304e67d8c3fed6bc
stefanooliveira <stefanocamposdeoliveira@gmail.com>
2023-09-02 12:39:25 -0300

    Configurando parametros para acessar o banco de dados e adicionando classes entre as APIs e o banco de produtos

--- src/main/java/com/pinguim/gerenciamentoprodutos/controller/GerencimentoController.java
+++ src/main/java/com/pinguim/gerenciamentoprodutos/controller/GerencimentoController.java
@@ -1,13 +1,21 @@
 package com.pinguim.gerenciamentoprodutos.controller;
 
 import com.pinguim.gerenciamentoprodutos.entity.Produto;
+import com.pinguim.gerenciamentoprodutos.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping ("/api/gerenciamento")
 public class GerencimentoController {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
 
     @GetMapping ("/hello")
     public ResponseEntity<Produto> helloWord(){
@@ -17,30 +25,31 @@ public class GerencimentoController {
         produto.setFabricante("Brastemp");
         return new ResponseEntity<>(produto, HttpStatus.OK);
     }
+    @GetMapping ("/listar")
+    public List<Produto> listarProdutos(){
+        return produtoRepository.buscarAtivos();
+    }
 
-    @PostMapping
+    @PostMapping("/cadastrar")
     public String cadastrarProduto(@RequestBody Produto produto){
-
+        produtoRepository.save(produto);
         return "Cadastrado com sucesso";
     }
 
-    @PutMapping("/alterar/{codigo}")
-    public String editarProduto(@RequestBody Produto produto, @PathVariable("codigo") int codigo){
+    @PutMapping("/alterar")
+    public String editarProduto(@RequestBody Produto produto){
+        produtoRepository.save(produto);
         return "Produto alterado com sucesso";
     }
 
     @DeleteMapping("/excluir/{codigo}")
-    public String excluirProduto(@PathVariable("codigo") int codigo){
+    public String excluirProduto(@PathVariable("codigo") Long codigo){
+        produtoRepository.deleteById(codigo);
         return "Produto excluído com sucesso";
     }
 
-    @GetMapping("/consultar/")
-    public Produto consultaProduto(){
-        Produto produto = new Produto();
-        produto.setModelo("Frost Free French Door A+++ - BRO85AK");
-        produto.setAno(2022);
-        produto.setFabricante("Brastemp");
-        produto.setCategoria("Refrigerador");
-        return produto;
+    @GetMapping("/consultar/{codigo}")
+    public Optional<Produto> consultaProduto(@PathVariable("codigo") Long codigo){
+        return produtoRepository.findById(codigo);
     }
 }
---
+++ src/main/java/com/pinguim/gerenciamentoprodutos/repository/ProdutoRepository.java
@@ -0,0 +1,28 @@
+package com.pinguim.gerenciamentoprodutos.repository;
+
+import com.pinguim.gerenciamentoprodutos.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    List<Produto> nomeContainingOrderByNomeDesc(String nome);
+
+    @Query("Select k from Produto k where k.nome like ?1")
+    List<Produto> buscaPorNome(String nome);
+    
+    @Query("Select k from Produto k where k.ativo = 1")
+    List<Produto> buscarAtivos();
+    
+    @Query(value = "Select count(*) from Produto", nativeQuery = true)
+    Integer countProduto();
+
+    Produto codigo(Long codigo);
+
+    @Query(value = "Select * from Produto", nativeQuery = true)
+    List<Produto> buscaPorNomeNativo(String nome);
+
+    
+}
