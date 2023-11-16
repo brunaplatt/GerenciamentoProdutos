21f7d12c563ae6d679c0ff6f2a36efa65d1b06df
Bruna Platt <bruna.platt@hotmail.com>
2023-11-16 20:32:52 -0300

    Atualizações Front End

--- src/main/java/FrontEnd/script.js
+++ src/main/java/FrontEnd/script.js
@@ -2,22 +2,26 @@ function login() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-// Simples verificação de usuário e senha
-    if (username === 'usuario' && password === 'senha') {
-        alert('Login bem-sucedido!');
+    var autorizado = (username === 'admin' && password === 'admin123');
+
+    if (autorizado) {
+        redirecionarUsuario(username);
     } else {
-        alert('Usuário ou senha incorretos. Tente novamente.');
+        alert('Credenciais inválidas');
+    }
+}
+
+function redirecionarUsuario(username) {
+    if (username === 'admin') {
+        window.location.href = 'usuarioadmin.html';
+    } else {
+        window.location.href = 'usuario.html';
     }
 }
 
 function abrirNovaJanela() {
     var novaJanela = window.open('redefinirsenha.html', '_blank', 'width=400, height=300');
-        novaJanela.addEventListener('load', function () {
-
-            novaJanela.fecharJanela = function () {
-                novaJanela.
-                close();
-            };
+        novaJanela.addEventListener('load', function (){
         });
 }
 
@@ -25,7 +29,7 @@ function resetPassword() {
     var newPassword = document.getElementById('new-password').value;
     var confirmPassword = document.getElementById('confirm-password').value;
 
-        if (newPassword === '' || confirmPassword === '') {
+        if (isCampoVazio(newPassword) || isCampoVazio(confirmPassword)) {
             alert('Por favor, preencha todos os campos.');
             return;
         }
@@ -34,24 +38,30 @@ function resetPassword() {
             alert('As senhas não coincidem. Por favor, verifique e tente novamente.');
             return;
         }
+        // Definir lógica de alteração de senha, junto com o Banco de Dados.
+        if (newPassword === confirmPassword){
+            alert('Senha redefinida com sucesso!');
+        }
+}
 
-             alert('Senha redefinida com sucesso!');
+function isCampoVazio(campo) {
+    return campo.trim() === '';
 }
 
 function senhaConfirmada() {
-    window.opener.fecharJanela();
+    window.close();
 }
 
-    window.onscroll = function () { scrollFunction() };
-    function scrollFunction() {
-        var btnTopo = document.getElementById("btnTop");
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            btnTopo.style.display = "block";
-        } else {
-            btnTopo.style.display = "none";
-        }
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    var btnTopo = document.getElementById("btnTop");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btnTopo.style.display = "block";
+    } else {
+        btnTopo.style.display = "none";
     }
-    function scrollToTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
+}
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
